e14c6ecc0a59bd5306c15027a89b09ab43cacce3
IshKevin <carterk279@gmail.com>
2023-01-31 14:42:49 +0200

    restyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import './HeaderStyle.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='head'>
+        <div className='logo'>
+        <h1>The Safari Blog</h1>
+        </div>
+        <div className='nav'>
+            <a href='#'>Sign up</a>
+            <a href='#'>Login</a>
+         </div>   
+    </div>
   )
 }
 

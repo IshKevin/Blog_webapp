bc37b460b453e0475df2c9e96b788a1061230d25
IshKevin <carterk279@gmail.com>
2023-01-31 14:37:18 +0200

    fixing error in welcome comelcome component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
     <Header/>
-    <h1>App</h1>
+    {/* <h1>App</h1>
+    <Welcome /> */}
     </div>
   );
 }

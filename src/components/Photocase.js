708a6c32e4062ca5983df72db2c7520ac3f260cd
IshKevin <carterk279@gmail.com>
2023-02-01 16:03:39 +0200

    make more photocase

--- src/components/Photocase.js
+++ src/components/Photocase.js
@@ -3,14 +3,16 @@ import React from 'react'
 const Photocase = () => {
   return (
     <div>
-      <img src="Images/pexels-photo-103123.jpeg" alt="Photocase" />
+      <div className="post1">
+      <img src={process.env.PUBLIC_URL + '/Images/Rain-Wallpaper-6004.jpg'} alt="Photocase" />
       <div className="postInfo">
         <div className="post_cats">
-          <span className="post_cat">Photography</span>
-          <span className="post_cat">Photocase</span>
+          <span className="post_cat">FLIM</span>
+          <span className="post_cat">PHOTO</span>
         </div>
         <span className='postTitle'>making perfect image</span>
         <span className="postDate">Posted on 12th May, 2018</span>
+        </div>
         </div>
     </div>
   )

95ed7b33ce12c0ad268e3112354a9d69d5385684
IshKevin <carterk279@gmail.com>
2023-02-01 16:02:39 +0200

    make photocase

--- src/components/Photocase.js
+++ src/components/Photocase.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Photocase = () => {
   return (
     <div>
-      <h1>Photocase</h1>
+      <img src="Images/pexels-photo-103123.jpeg" alt="Photocase" />
+      <div className="postInfo">
+        <div className="post_cats">
+          <span className="post_cat">Photography</span>
+          <span className="post_cat">Photocase</span>
+        </div>
+        <span className='postTitle'>making perfect image</span>
+        <span className="postDate">Posted on 12th May, 2018</span>
+        </div>
     </div>
   )
 }

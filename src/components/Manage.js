87d93cf7a24eba816e11004632aaafc289178394
IshKevin <carterk279@gmail.com>
2023-02-02 17:14:56 +0200

    finsh style of managesection

--- src/components/Manage.js
+++ src/components/Manage.js
@@ -1,20 +1,55 @@
 import React from 'react'
 import './Manage.css'
-
+import {AiOutlineEdit} from 'react-icons/ai'
+import {MdOutlineDelete} from 'react-icons/md'
 const Manage = () => {
   return (
-    <div>
+    <div className='manage-blog'>
         <div className='title-head'>
           <div>Title</div>
-          <div>Action</div>
+          <div className='act-title'>Action</div>
         </div>
         <div className='title-post'>
-          <div>Post 1</div>
-          <div>
-            <div> AiOutlineEdit</div>
-            <div>log2</div>
+          <div>making perfect image</div>
+          <div className='icons'>
+            <div className='icon1' > <AiOutlineEdit/></div>
+            <div><MdOutlineDelete/></div>
           </div>
-
+        </div>
+        <div className='title-post'>
+          <div>making perfect image</div>
+          <div className='icons'>
+            <div className='icon1' > <AiOutlineEdit/></div>
+            <div><MdOutlineDelete/></div>
+          </div>
+        </div>
+        <div className='title-post'>
+          <div>making perfect image</div>
+          <div className='icons'>
+            <div className='icon1' > <AiOutlineEdit/></div>
+            <div><MdOutlineDelete/></div>
+          </div>
+        </div>
+        <div className='title-post'>
+          <div>making perfect image</div>
+          <div className='icons'>
+            <div className='icon1' > <AiOutlineEdit/></div>
+            <div><MdOutlineDelete/></div>
+          </div>
+        </div>
+        <div className='title-post'>
+          <div>making perfect image</div>
+          <div className='icons'>
+            <div className='icon1' > <AiOutlineEdit/></div>
+            <div><MdOutlineDelete/></div>
+          </div>
+        </div>
+        <div className='title-post'>
+          <div>making perfect image</div>
+          <div className='icons'>
+            <div className='icon1' > <AiOutlineEdit/></div>
+            <div><MdOutlineDelete/></div>
+          </div>
         </div>
     </div>
   )

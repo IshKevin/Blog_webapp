6ab1de1f4db41729f8d8fc4ecec0602ba61c2417
IshKevin <carterk279@gmail.com>
2023-02-02 10:53:41 +0200

    crete function for manage page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './Dashboard.css'
 
 const Dashboard = () => {
+    const displaymanager = () => {
+        console.log('manage')
+    }
   return (
     <div>
        <div className='heads'>
          <h1>Dashboard</h1>
          </div>
          <div div className ="dash">
-            <button className='button-dash'>Manage</button><br/>
+            <button className='button-dash' onClick={displaymanager}>Manage</button><br/>
             <button className='button-dash'>New Post</button>
         </div>
     </div>

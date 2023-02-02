335e87f09a95998d0126fceaa6638c9f627bd932
IshKevin <carterk279@gmail.com>
2023-02-02 18:39:38 +0200

    style input type of from

--- src/components/Newpost.js
+++ src/components/Newpost.js
@@ -3,14 +3,14 @@ import './Newpost.css'
 
 const Newpost = () => {
   return (
-    <div>
-        <from>
+    <div className="Post-form">
+        <from className="form-manager" >
           <label for='title'>Title</label><br/>
           <input type='text' id='title' name='title' placeholder='Title'/><br/>
           <label for='content'>Uplaod image</label><br/>
           <input type='file' id='content' name='content' placeholder='Upload image'/><br/>
           <label for='content'>Content</label><br/>
-          <textarea id='content' name='content' placeholder='Content'></textarea><br/>
+          <textarea id='content' name='content' placeholder='Content' heigth='30px'></textarea><br/>
           <button type='submit' value='Post'>Post</button>
 
         </from>

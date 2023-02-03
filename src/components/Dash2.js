c27e7a06a221d8e0c1c3ba21bff08a8e4da4cfcf
IshKevin <carterk279@gmail.com>
2023-02-03 09:22:23 +0200

    clean code

---
+++ src/components/Dash2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dash2 = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Dash2
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,20 +3,8 @@ import './Dashboard.css'
 import Manage from './Manage'
 import {NavLink} from 'react-router-dom'
 
-const Dashboard = (Children) => {
-    // const menuitems = [{
-    //     path:'/manage',
-    //     name:'Manage'
-    //      },
-    //     {
-    //     path:'/newpost',
-    //     name:'New Post'
-    //     }
-
-    // ]
-    // const displaymanager = () => {
-    //    return <Manage/>
-    // }
+const Dashboard = () => {
+    
   return (
     <div className='dash-disp'>
        <header className='heads'>
@@ -26,15 +14,6 @@ const Dashboard = (Children) => {
          <div div className ="dash">
             <button className='button-dash'>Manage</button><br/>
             <button className='button-dash'>New Post</button>
-           {/* <div>
-              {menuitems.map((item, index) => (
-              <NavLink to={item.path} key={index}>
-              {item.name}
-              <div className='line'>
-                {item.name}
-              </div>
-              </NavLink>
-            ))} */}
           </div>
           <div className='man-disp'>
           <Manage/>

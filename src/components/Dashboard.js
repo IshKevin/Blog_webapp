0655eb5ad2a4ae8868bf13e961b41fdb567c3bc2
IshKevin <carterk279@gmail.com>
2023-02-03 09:20:16 +0200

    redesign dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,28 +4,29 @@ import Manage from './Manage'
 import {NavLink} from 'react-router-dom'
 
 const Dashboard = (Children) => {
-    const menuitems = [{
-        path:'/manage',
-        name:'Manage'
-         },
-        {
-        path:'/newpost',
-        name:'New Post'
-        }
+    // const menuitems = [{
+    //     path:'/manage',
+    //     name:'Manage'
+    //      },
+    //     {
+    //     path:'/newpost',
+    //     name:'New Post'
+    //     }
 
-    ]
-    const displaymanager = () => {
-       return <Manage/>
-    }
+    // ]
+    // const displaymanager = () => {
+    //    return <Manage/>
+    // }
   return (
-    <div>
+    <div className='dash-disp'>
        <header className='heads'>
          <h1>Dashboard</h1>
          </header>
+         <body className='body'>
          <div div className ="dash">
-            {/* <button className='button-dash' onClick={displaymanager}>Manage</button><br/>
-            <button className='button-dash'>New Post</button> */}
-           <div>
+            <button className='button-dash'>Manage</button><br/>
+            <button className='button-dash'>New Post</button>
+           {/* <div>
               {menuitems.map((item, index) => (
               <NavLink to={item.path} key={index}>
               {item.name}
@@ -33,15 +34,14 @@ const Dashboard = (Children) => {
                 {item.name}
               </div>
               </NavLink>
-            ))}
+            ))} */}
           </div>
-          
-
-        </div>
-        {/* <main>
-            {Children}
-          </main> */}
+          <div className='man-disp'>
+          <Manage/>
+          </div>
+          </body>
     </div>
+
   )
 }
 

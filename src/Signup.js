21eca2ed8afaf5b703683ae91f9cb4c72e933a0b
IshKevin <carterk279@gmail.com>
2023-02-06 20:28:32 +0200

    styling sign up

--- src/Signup.js
+++ src/Signup.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
 import Signupform from './components/Signupform'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const Signup = () => {
   return (
     <div>
-      <Header />
+     <Navbar/>
       <Signupform />
+      <Footer/>
     </div>
   )
 }

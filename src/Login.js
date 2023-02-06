43f3521d38da4f225226fad76df30eea9e823229
IshKevin <carterk279@gmail.com>
2023-02-06 20:29:17 +0200

    styling log in

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Loginform from './components/Loginform'
+import Navbar from './components/Navbar'
 
 const Login = () => {
   return (
     <div>
+        <Navbar/>
         <Loginform/>
+        <Footer/>
     </div>
   )
 }

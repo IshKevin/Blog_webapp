6a448da0a9ae47f59df59aaf837ab61ac75d402e
IshKevin <carterk279@gmail.com>
2023-02-06 20:31:21 +0200

    clean code

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Footer from './components/Footer'
-import Header from './components/Header'
 import Loginform from './components/Loginform'
 import Navbar from './components/Navbar'
 

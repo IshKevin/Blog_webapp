c1fd61dbb338524ee94e10f7d67c0478c29fe70e
IshKevin <carterk279@gmail.com>
2023-02-06 10:55:33 +0200

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>welcome to footer</div>
+  )
+}
+
+export default Footer

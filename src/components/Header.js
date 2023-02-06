430d60439580a5a092e72f4ee74e60f01e69f532
IshKevin <carterk279@gmail.com>
2023-02-06 16:00:37 +0200

    style head

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import './HeaderStyle.css'
 const Header = () => {
   return (
     <div className='head'>
-     <div className='welcome-text-header'>
+     <div className='text-header'>
      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.

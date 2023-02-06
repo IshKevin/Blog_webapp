941f04d4d0abb7a29ffcae1b6ff5250c60b1501f
IshKevin <carterk279@gmail.com>
2023-02-06 10:50:06 +0200

    add header

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 import Photocase from './components/Photocase'
 import Welcome from './components/Welcome'
 
 const Home = () => {
   return (
     <div>
+      <React.Fragment>
+    <Navbar/>
     <Header className="hheader"/>
     <Photocase/>
+    </React.Fragment>
     </div>
   )
 }

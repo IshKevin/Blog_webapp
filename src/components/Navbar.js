8ca838b38607e508251b25d39dd092d6e8ce2033
IshKevin <carterk279@gmail.com>
2023-02-06 12:22:11 +0200

    styling header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+import { FiMenu } from 'react-icons/fi'
+import { FaTimes } from 'react-icons/fa'
+
 
 const Navbar = () => {
+    const navRef = useRef();
+    const toggleNav = () => {
+        navRef.current.classList.toggle('responsive_nav');
+    }
   return (
-    <div>new navigation system </div>
+    <header className='Header-Style'>
+        <div className="logo">
+            <span className='logo-name'>The Safari Blog</span>    
+        </div>
+        <nav ref={navRef}>
+            <Link to='Signup'>Signup</Link>
+            <Link to='Login'>Login</Link>
+            <button className='nav-btn nav-close-btn' onClick={toggleNav}>
+            <FaTimes />
+            </button>
+        </nav>
+        <button className='nav-btn' onClick={toggleNav}>
+            <FiMenu/>
+        </button>
+    </header>
   )
 }
 

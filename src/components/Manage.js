4e985a04b463e1f216126f8bf11074be13aa728d
IshKevin <carterk279@gmail.com>
2023-02-09 14:35:44 +0200

    fixing dashboard

--- src/components/Manage.js
+++ src/components/Manage.js
@@ -6,46 +6,65 @@ const Manage = () => {
   return (
     <div className='manage-blog'>
         <div className='title-head'>
+          <div>Avatar</div>
           <div>Title</div>
           <div className='act-title'>Action</div>
         </div>
         <div className='title-post'>
-          <div>making perfect image</div>
+        <div class="avatar">
+           <img class="avatar__image" src={process.env.PUBLIC_URL + '/Images/pexels-pixabay-34098.jpg'} />
+           </div> 
+          <div className='title-on-post'>making perfect image</div>
           <div className='icons'>
             <div className='icon1' > <AiOutlineEdit/></div>
             <div><MdOutlineDelete/></div>
           </div>
         </div>
         <div className='title-post'>
-          <div>making perfect image</div>
+        <div class="avatar">
+           <img class="avatar__image" src={process.env.PUBLIC_URL + '/Images/pexels-pixabay-34098.jpg'} />
+           </div> 
+          <div className='title-on-post'>making perfect image</div>
           <div className='icons'>
             <div className='icon1' > <AiOutlineEdit/></div>
             <div><MdOutlineDelete/></div>
           </div>
         </div>
         <div className='title-post'>
-          <div>making perfect image</div>
+        <div class="avatar">
+           <img class="avatar__image" src={process.env.PUBLIC_URL + '/Images/pexels-pixabay-34098.jpg'} />
+           </div> 
+          <div className='title-on-post'>making perfect image</div>
           <div className='icons'>
             <div className='icon1' > <AiOutlineEdit/></div>
             <div><MdOutlineDelete/></div>
           </div>
         </div>
         <div className='title-post'>
-          <div>making perfect image</div>
+        <div class="avatar">
+           <img class="avatar__image" src={process.env.PUBLIC_URL + '/Images/pexels-pixabay-34098.jpg'} />
+           </div> 
+          <div className='title-on-post'>making perfect image</div>
           <div className='icons'>
             <div className='icon1' > <AiOutlineEdit/></div>
             <div><MdOutlineDelete/></div>
           </div>
         </div>
         <div className='title-post'>
-          <div>making perfect image</div>
+        <div class="avatar">
+           <img class="avatar__image" src={process.env.PUBLIC_URL + '/Images/pexels-pixabay-34098.jpg'} />
+           </div> 
+          <div className='title-on-post'>making perfect image</div>
           <div className='icons'>
             <div className='icon1' > <AiOutlineEdit/></div>
             <div><MdOutlineDelete/></div>
           </div>
         </div>
         <div className='title-post'>
-          <div>making perfect image</div>
+        <div class="avatar">
+           <img class="avatar__image" src={process.env.PUBLIC_URL + '/Images/pexels-pixabay-34098.jpg'} />
+           </div> 
+          <div className='title-on-post'>making perfect image</div>
           <div className='icons'>
             <div className='icon1' > <AiOutlineEdit/></div>
             <div><MdOutlineDelete/></div>

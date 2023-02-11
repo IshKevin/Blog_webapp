f0fba4e730292ab09600a508c7c7f5b22405491b
IshKevin <carterk279@gmail.com>
2023-02-11 14:31:46 +0200

    redesigin post dashboard

--- src/components/Newpost.js
+++ src/components/Newpost.js
@@ -3,19 +3,8 @@ import './Newpost.css'
 
 const Newpost = ({posts}) => {
   return (
-    <div className="Post-form">
-        <from className="form-manager" >
-          <label className='label'for='title'>Title</label><br/>
-          <input className='input'type='text' id='title' name='title' placeholder='Title'/><br/>
-          <label className='label' for='content'>Uplaod image</label><br/>
-          <input className='input'type='file' id='content' name='content' placeholder='Upload image'/><br/>
-          <label className='label'for='content'>Content</label><br/>
-          <textarea className='textarea' id='content' name='content' placeholder='Content' heigth='30px'></textarea><br/>
-          <button className='button'type='submit' value='Post'>Post</button>
-
-        </from>
-    </div>
+    <>
+   
+    </>
   )
 }
-
-export default Newpost

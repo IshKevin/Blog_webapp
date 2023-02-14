a47ceeb753e4ae3e43b59312856091d4b0d333cd
IshKevin <carterk279@gmail.com>
2023-02-14 15:29:41 +0200

    restyle header of dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,30 +11,37 @@ const Dashboard = ({posts}) => {
   return (
     <div className='dash-disp'>
        <header className='heads'>
+        <div>
+       <h1>Dashboard</h1>
+        </div>
+        <div>
         <button>
         <Link to='/'>
          <AiFillHome className='icon-home'/>
          </Link>
         </button>
-         <h1>Dashboard</h1>
+        </div>
+         
          </header>
          <div className="sidebar">
          <aside className="sidebar__sidebar">
          <div div className ="dash">
          <button className='button-dash'>
               <Link className="button-dash" to='../Dash2disp'>
-              <h1 className='button-title'>Home
+              <h1 className='button-title'>Home</h1>
                 </Link> 
                 </button><br/>          
-            <button className='button-dash'>Manage</button>
+            <button className='button-dash'>
+            <h1 className='button-title'>Manage</h1>
+            </button>
             <button className='button-dash'>
               <Link className="button-dash" to='../Dash2disp'>
-                New Post
+              <h1 className='button-title'>New Post</h1>
                 </Link> 
                 </button>
                 <button className='button-dash'>
               <Link className="button-dash" to='/'>
-                Logout
+              <h1 className='button-title'>Logout</h1>
                 </Link> 
                 </button>    
           </div>

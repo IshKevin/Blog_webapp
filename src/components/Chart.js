a694b0f9369b83c480a2802c800c256d460d88cb
IshKevin <carterk279@gmail.com>
2023-02-16 14:27:23 +0200

    add new chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,7 +1,50 @@
 import React from 'react'
 import './Chart.css'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { AreaChart, Area } from 'recharts';
+
 
 const Chart = () => {
+
+  const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+ 
+];
   return (
     <div className='Chart'>
     <div className='dash-home-title'> WELCOME TO DASHBOARD</div>
@@ -18,26 +61,85 @@ const Chart = () => {
         post that consist of<br/> 
          a single impression.</p>
        </div>
-       <div>
-        Chart
+       <div className="chart-bar">
+           <BarChart className="chart-bar" width={200} height={110} data={data}>
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
         </div>   
         </div>
 
     </div>
     <div className="card-layout__item">
+        <div className="card-inside">
+        <div>
         <h2>4,868</h2>
         <h5>View Through</h5>
-        <p> Estimated daily unique 
-            views per visitor on the posts.</p>
+        <p className='card-exp'> Estimated daily unique <br/> 
+            views per visitor <br/>
+            on the posts.</p>
+            </div>
+       <div className="chart-bar">
+           <BarChart className="chart-bar" width={200} height={110} data={data}>
+          <Bar dataKey="pv" fill="#8884d8" overflow={'hidden'} />
+        </BarChart>
+        </div>   
+        </div>    
     </div>
     <div class="card-layout__item">
+        <div className="card-inside">
+        <div> 
         <h2>8,785</h2>
         <h5>Total Conversions</h5>
-        <p> Estimated total conversions on 
-            post per impressions.</p>
+        <p className='card-exp'>Estimated total<br/> 
+        conversions on<br/> 
+          post per impressions.</p>
+            </div>
+       <div className="chart-bar">
+           <BarChart className="chart-bar" width={200} height={110} data={data}>
+          <Bar dataKey="pv" fill="#8884d8" overflow={'hidden'} />
+        </BarChart>
+        </div>   
+        </div>      
     </div>
     </div>
     </div>
+
+    <div className='dash-home-sect2'>
+        <div>
+        <h2 className=''>Organic Visits & Conversions</h2>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        </AreaChart>
+        </div>
+       <div>
+       <h2 className=''> Analytics Performance</h2>
+        </div>
+      </div> 
+
+      <div>
+        egreh
+        Fgrhg
+        
+        
+        rhthfbhstjjjjjjjjjjjjjjjjjjjjnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+        gerhn</div>  
+
 
     </div>
   )

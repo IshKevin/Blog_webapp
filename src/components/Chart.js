446c6f8862eb9c7a484bab348e901ec96dfd2727
IshKevin <carterk279@gmail.com>
2023-02-16 11:31:34 +0200

    re style single card

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -8,15 +8,23 @@ const Chart = () => {
 
 
     <div className='dash-home-sect1'> 
-    <div class="card-layout">
-    <div class="card-layout__item">
+    <div className="card-layout">
+    <div className="card-layout__item">
+        <div className="card-inside">
+        <div>
         <h2>4,608</h2>
         <h5>Click Through</h5>
-        <p> No. of clicks to post that consist of 
-            a single impression.</p>
+        <p className='card-exp'> No. of clicks to<br/> 
+        post that consist of<br/> 
+         a single impression.</p>
+       </div>
+       <div>
+        Chart
+        </div>   
+        </div>
 
     </div>
-    <div class="card-layout__item">
+    <div className="card-layout__item">
         <h2>4,868</h2>
         <h5>View Through</h5>
         <p> Estimated daily unique 

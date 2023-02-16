92d7bcd5c1e70aa46dfcd20c20b76f133d451c97
IshKevin <carterk279@gmail.com>
2023-02-16 14:50:21 +0200

    fix heigt

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -8,37 +8,37 @@ const Chart = () => {
 
   const data = [
   {
-    name: 'Page A',
+    name: 'Jan',
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: 'Feb',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: 'Mar',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: 'Apr',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'May',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'Jun',
     uv: 2390,
     pv: 3800,
     amt: 2500,
@@ -108,20 +108,20 @@ const Chart = () => {
         <div>
         <h2 className=''>Organic Visits & Conversions</h2>
         <AreaChart
-          width={500}
-          height={400}
+          className="chart-bar" 
+          width={600}
+          height={300}
           data={data}
           margin={{
-            top: 10,
+            top: 60,
             right: 30,
-            left: 0,
-            bottom: 0,
+            left: 20,
+            bottom: 10,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
+          
+          <XAxis  dataKey="name" />
+          <YAxis  />
           <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
           <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
           <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
@@ -131,14 +131,6 @@ const Chart = () => {
        <h2 className=''> Analytics Performance</h2>
         </div>
       </div> 
-
-      <div>
-        egreh
-        Fgrhg
-        
-        
-        rhthfbhstjjjjjjjjjjjjjjjjjjjjnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-        gerhn</div>  
 
 
     </div>

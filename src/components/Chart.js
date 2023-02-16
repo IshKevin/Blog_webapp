6b8f320e583e5b5001a3474572d2de9d7a539fb5
IshKevin <carterk279@gmail.com>
2023-02-16 11:14:56 +0200

    add card info

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -9,9 +9,25 @@ const Chart = () => {
 
     <div className='dash-home-sect1'> 
     <div class="card-layout">
-    <div class="card-layout__item">card 1</div>
-    <div class="card-layout__item">card 2</div>
-    <div class="card-layout__item">card 3</div>
+    <div class="card-layout__item">
+        <h2>4,608</h2>
+        <h5>Click Through</h5>
+        <p> No. of clicks to post that consist of 
+            a single impression.</p>
+
+    </div>
+    <div class="card-layout__item">
+        <h2>4,868</h2>
+        <h5>View Through</h5>
+        <p> Estimated daily unique 
+            views per visitor on the posts.</p>
+    </div>
+    <div class="card-layout__item">
+        <h2>8,785</h2>
+        <h5>Total Conversions</h5>
+        <p> Estimated total conversions on 
+            post per impressions.</p>
+    </div>
     </div>
     </div>
 

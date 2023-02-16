3f3868af98d6047e1d935c4681a3ec0f47f94dd0
IshKevin <carterk279@gmail.com>
2023-02-16 10:58:26 +0200

    style general chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './Chart.css'
 
 const Chart = () => {
   return (
-    <>
-    <h2 className='dash-home-title'> WELCOME TO DASHBOARD</h2>    
-    </>
+    <div className='Chart'>
+    <div className='dash-home-title'> WELCOME TO DASHBOARD</div>
+
+
+    <div className='dash-home-sect1'> 
+    <div class="card-layout">
+    <div class="card-layout__item">card 1</div>
+    <div class="card-layout__item">card 2</div>
+    <div class="card-layout__item">card 3</div>
+    </div>
+    </div>
+
+    </div>
   )
 }
 
